a1dbed9599078a6161aa876152e148f2c72b0dc8
Eugene <zhekazuev@gmail.com>
2017-10-24 19:59:25 +0300

    Подключили пространство имен для нашей нугет библиотеки
    Подключили пространство имен для нашей нугет библиотеки

--- MonoTorrent/MonoTorrent/Program.cs
+++ MonoTorrent/MonoTorrent/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using MonoTorrent.Client;//Подключили пространство имен для нашей нугет библиотеки
+using MonoTorrent.Common;//Подключили пространство имен для нашей нугет библиотеки
 using System.Text;
 using System.Threading.Tasks;
 
