e7ef48b8f2675e26e0b1cce3ba6975573d433ec0
Eugene <zhekazuev@gmail.com>
2017-10-24 22:10:23 +0300

    Задание 1 Методы
    Выполнил задание 1 по методам.

---
+++ Lesson #1/008_Methods/001_Methods/HomeWork_8_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Используя Visual Studio, создайте проект по шаблону Console Application.
+//Создайте метод с именем Calculate, который принимает в качестве параметров три целочисленных значения
+//и возвращает значение каждого аргумента деленного на 5.  
+
+
+namespace HomeWork_8_1
+{
+	class Program
+	{
+		static float Calculate(int a, int b, int c)
+		{
+			
+			float d = a / 5f;
+
+
+			float f = b / 5f;
+
+
+			float g = c / 5f;
+
+			Console.WriteLine("a / 5 = {0}  b / 5 = {1} c / 5 = {2}" , d, f, g);
+			return 0f;
+			
+		}
+
+		static void Main(string[] args)
+		{
+			
+			
+			Console.WriteLine(Calculate(5,6,7));
+			Console.ReadKey();
+		}
+	}
+}
