3b2cdd3e2d94d58415f7d86180699c5e86ea4d38
Eugene <zhekazuev@gmail.com>
2017-11-14 16:22:34 +0300

    Дабавил всякую фигню.Пока не работает

---
+++ Lesson #3/002_Classes/002_Classes/HomeWork_1_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Создать класс Converter.
+//В теле класса создать пользовательский конструктор, который принимает три вещественных аргумента, 
+//и инициализирует поля соответствующие курсу 3-х основных валют, по отношению к гривне – public Converter(double usd, double eur, double rub).  
+//Написать программу, которая будет выполнять конвертацию из гривны в одну из указанных валют,
+//также программа должна производить конвертацию из указанных валют в гривну.
+
+namespace HomeWork_1_1
+{
+	class Converter
+	{
+		public double USD;
+		public double EUR;
+		public double RUB;
+
+		public Converter(double usd,double eur,double rub)
+		{
+			this.USD = usd;
+			this.EUR = eur;
+			this.RUB = rub;
+		}
+		public double ConvUSD(double grivna)
+		{
+			double convUSD = 3 * grivna;
+			return convUSD;
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
--- MonoTorrent/MonoTorrent/Program.cs
+++ MonoTorrent/MonoTorrent/Program.cs
@@ -8,17 +8,70 @@ using System.Threading.Tasks;
 
 namespace MonoTorrent
 {
-	class Program
+	internal static class Program
 	{
-		
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
-			
-			Torrent torrent = Torrent.Load(torrentFilePath);//Для получения информации о torrent-файле и последующей его загрузки
-			TorrentManager manager = new TorrentManager(torrent, downloadFolderPath, new TorrentSettings());//для управления одним торрентом 
-			ClientEngine engine = new ClientEngine(new EngineSettings());//необходимый для осуществления контроля и управления за всеми торрентами и клиента в общем
-			engine.Register(manager);//отслеживание движком нашего менеджера
-			manager.Start();//для начала загрузки файлов
+			if (args.Length != 2)
+			{
+				Console.WriteLine("Incorrect arguments given to input!");
+			}
+
+			var torrentFilePath = args[0];
+			var downloadFolderPath = args[1];
+
+			Downloader(torrentFilePath,downloadFolderPath);
+			Console.ReadKey();
+		}
+
+		private static void Downloader(string torrentFilePath, string downloadFolderPath)
+		{
+			Torrent torrent = Torrent.Load(torrentFilePath);//управления одним торрентом 
+
+			ClientEngine engine = new ClientEngine(new EngineSettings());//настройки самого движка нашего клиента, для начала пренебрегая этими настройками. 
+			TorrentManager manager = new TorrentManager(torrent, downloadFolderPath, new TorrentSettings());//осуществления контроля и управления за всеми торрентами
+
+			engine.Register(manager);//отслеживание движком нашего менеджера 
+			manager.Start();//начала загрузки файлов
 		}
 	}
+
 }
+
+//Console.WriteLine("Выберите папку из списка ниже, откуда нужно взять торрент.Желательно выбрать путь 1.");
+//Console.WriteLine("1 - D:/Torrent/c#.torrent");
+//Console.WriteLine("2 - Путь отсутствует");
+//string a = Console.ReadLine();
+
+//switch (a)
+//{
+//	case "1":
+//		string torrentFilePath = @"D:\Torrent\c#.torrent";
+//		break;
+//	case "2":
+//		string torrentFilePath = @"пока тут пусто";
+//		break;
+//	default:
+//		Console.WriteLine("Default case");
+//		break;
+//}
+
+//Console.WriteLine("Выберите папку из списка ниже, куда нужно записать файл.Желательно выбрать путь 1.");
+//Console.WriteLine("1 - D:/Torrent");
+//Console.WriteLine("2 - Путь отсутствует");
+//string b = Console.ReadLine();
+
+//switch (b)
+//{
+//	case "1":
+//		string downloadFolderPath = @"D:\Torrent";
+//		break;
+//	case "2":
+//		string downloadFilePath = @"пока тут пусто";
+//		break;
+//	default:
+//		Console.WriteLine("Default case");
+
+//		break;
+
+//}
