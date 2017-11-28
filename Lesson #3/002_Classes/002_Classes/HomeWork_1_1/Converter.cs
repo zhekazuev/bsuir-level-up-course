d72589077cb686e617ee615a5285da6a82ebd1b5
Eugene <zhekazuev@gmail.com>
2017-11-28 23:41:20 +0300

    Задание 1 про конвертер.

---
+++ Lesson #3/002_Classes/002_Classes/HomeWork_1_1/Converter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_1_1
+{
+	class Converter
+	{
+		private readonly double USD;
+		private readonly double EUR;
+		private readonly double RUB;
+
+		public Converter(double usd, double eur, double rub)
+		{
+			this.USD = usd;
+			this.EUR = eur;
+			this.RUB = rub;
+		}
+
+		public double ConvUSD(double Grivna)
+		{
+			Grivna = USD/Grivna ;
+			return Grivna;
+		}
+
+		public double ConvEUR(double Grivna)
+		{
+			Grivna = EUR / Grivna;
+			return Grivna;
+		}
+
+		public double ConvRUB(double Grivna)
+		{
+			Grivna = RUB / Grivna;
+			return Grivna;
+		}
+	}
+}
--- Lesson #3/002_Classes/002_Classes/HomeWork_1_1/Program.cs
+++ Lesson #3/002_Classes/002_Classes/HomeWork_1_1/Program.cs
@@ -12,30 +12,45 @@ using System.Threading.Tasks;
 
 namespace HomeWork_1_1
 {
-	class Converter
-	{
-		public double USD;
-		public double EUR;
-		public double RUB;
-
-		public Converter(double usd,double eur,double rub)
-		{
-			this.USD = usd;
-			this.EUR = eur;
-			this.RUB = rub;
-		}
-		public double ConvUSD(double grivna)
-		{
-			double convUSD = 3 * grivna;
-			return convUSD;
-		}
-	}
-
 	class Program
 	{
 		static void Main(string[] args)
 		{
+S:			Console.WriteLine("Выберите в какую валюту вы хотите конвертировать гривны...");
+			Console.WriteLine("1.USD");
+			Console.WriteLine("2.EUR");
+			Console.WriteLine("3.RUB");
+			Console.WriteLine("?");
+			int a = int.Parse(Console.ReadLine());
 
+			Console.WriteLine("Введите сумму...");
+			int n = int.Parse(Console.ReadLine());
+
+			double Grivna = 1D;
+			double USD = 26.87 * Grivna;
+			double EUR = 31.97 * Grivna;
+			double RUB = 0.46 * Grivna;
+
+			Converter convert = new Converter(USD,EUR,RUB);
+					
+			switch(a)
+			{
+				case 1:
+					Console.WriteLine("{1} USD= {0} Grivna", n*convert.ConvUSD(Grivna),n);
+					Console.WriteLine("{1} Grivna = {0} USD",n * Grivna / convert.ConvUSD(Grivna), n);
+					break;
+				case 2:
+					Console.WriteLine("{1} EUR = {0} Grivna", n * convert.ConvEUR(Grivna), n);
+					Console.WriteLine("{1} Grivna  = {0} EUR", n * Grivna / convert.ConvEUR(Grivna), n);
+					break;
+				case 3:
+					Console.WriteLine("{1} RUB = {0} Grivna", n * convert.ConvRUB(Grivna), n);
+					Console.WriteLine("{1} Grivna  = {0} RUB", n * Grivna / convert.ConvRUB(Grivna), n);
+					break;
+			}
+			
+			goto S;
+			
 		}
 	}
 }
